365c879b4116e3f7718049675a180585fd2ba203
MaximeVlr <vilar.maxime@gmail.com>
2026-06-22 11:19:48 +0200

    feat: update routes to return static content for home, about, and projects pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return 'Accueil';
 });
+
+Route::get('/about', function() {
+    return 'À propos';
+});
+
+Route::get('/projects', function() {
+    return 'Liste des projets';
+});
+
+Route::get('/projects/{id}', function($id) {
+    return 'Détail du projet ' . $id;
+});
+
