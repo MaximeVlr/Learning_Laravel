87915a0b5fb6c5a0bb717f2f37b1f62714caaa27
MaximeVlr <vilar.maxime@gmail.com>
2026-06-26 11:08:00 +0200

    feat: Update project detail view to display project title and technologies

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mes projets</title>
-</head>
-<body>
-
-</body>
-</html>
-
 @extends('layouts.app')
 
-@section('title', 'Mes projets')
+@section('title', $project['title'])
 
 @section('content')
     <h1>Détail du projet</h1>
     
     <h2>{{ $project['title'] }}</h2>
     <p>{{ $project['description'] }}</p>
+    <p>Technologies :</p>
+    <ul>
+        @foreach ($project['technologies'] as $technology)
+            <li>{{ $technology }}</li>
+        @endforeach
+    </ul>
     <a href="{{ route('projects.index') }}">Retour à la liste</a>
 @endsection
