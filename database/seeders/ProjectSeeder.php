9e6644dce5e4654a9d88587e0c93f8170e6e2e30
MaximeVlr <vilar.maxime@gmail.com>
2026-06-29 15:46:56 +0200

    feat: Add technology entries and associate them with the Portfolio project in ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Database\Seeder;
 
 class ProjectSeeder extends Seeder
@@ -12,6 +13,29 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
+        $laravel = Technology::create([
+            'name' => 'Laravel',
+        ]);
+
+        $blade = Technology::create([
+            'name' => 'Blade',
+        ]);
+
+        $php = Technology::create([
+            'name' => 'PHP',
+        ]);
+
+        $portfolio = Project::create([
+            'title' => 'Portfolio Laravel',
+            'description' => 'Un projet pour présenter mes compétences en Laravel.',
+        ]);
+
+        $portfolio->technologies()->attach([
+            $laravel->id,
+            $blade->id,
+            $php->id,
+        ]);
+
         Project::factory()->count(10)->create();
     }
 }
